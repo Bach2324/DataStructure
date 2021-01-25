0f8b56d50d5ff26429a75650fe3bdf2c623c87f6
Bach Nguyen <73509508+Bach2324@users.noreply.github.com>
2021-01-25 16:54:31 -0700

    Reworking writing to file conditions

--- Assignment1/problem2.cpp
+++ Assignment1/problem2.cpp
@@ -3,30 +3,31 @@
 // Bach Nguyen
 
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <fstream>
 #include <sstream>
 
 using namespace std;
 
-struct studentData
+struct studentData                  // Declaring a struct of student data
 {
-    string studentName;
-    int homework;
-    int recitation;
-    int quiz;
-    int exam;
-    double average;
+    string studentName;             // Their name
+    int homework;                   // Homework score
+    int recitation;                 // Recitation score
+    int quiz;                       // Quiz score
+    int exam;                       // Exam score
+    double average;                 // Average of sum of all assignment score
 };
 
 void addStudentData(studentData students[], string studentName, int homework, int recitation, int quiz, int exam, int length)
 {
-    students[length].studentName = studentName;
-    students[length].homework = homework;
-    students[length].recitation = recitation;
-    students[length].quiz = quiz;
-    students[length].exam = exam;
-    students[length].average = (homework + recitation + quiz + exam)/(double)4; 
+    students[length].studentName = studentName;                                     // Adding the name of student into an array of struct
+    students[length].homework = homework;                                           // Adding Homework score of student an array of struct
+    students[length].recitation = recitation;                                       // Adding recitation score into an array of struct
+    students[length].quiz = quiz;                                                   // Adding quiz score into an array of struct
+    students[length].exam = exam;                                                   // Adding exam score into an array of struct
+    students[length].average = (homework + recitation + quiz + exam)/(double)4;     // Adding average score into an array of struct 
 
 return;
 }
@@ -88,20 +89,22 @@ int main(int argc, const char *argv[])
     ofstream outFile(argv[2]);
     double upper_bound, lower_bound;
 
+    string arg3 = argv[3], arg4 = argv[4];
+
     for (int i = 0; i < count; i++)
     {
-        if (strcmp(argv[4], "A") == 0)
+        if (arg4 == "A")
         {
             upper_bound = (double)100;
-            if (strcmp(argv[3], "B") == 0)
+            if(arg3 == "B")
             {
                 lower_bound = (double)80;
             }
-            else if (strcmp(argv[3], "C") == 0)
+            else if (arg3 == "C")
             {
                 lower_bound = (double)70;
             }
-            else if(strcmp(argv[3], "D") == 0)
+            else if(arg3 == "D")
             {
                 lower_bound = (double)60;
             }
@@ -110,14 +113,14 @@ int main(int argc, const char *argv[])
                 lower_bound = (double)0;
             }
         }
-        else if (strcmp(argv[4], "B") == 0)
+        else if (arg4 == "B")
         {
             upper_bound = 89.9;
-            if (strcmp(argv[3], "C") == 0)
+            if (arg3 == "C")
             {
                 lower_bound = (double)70;
             }
-            else if(strcmp(argv[3], "D") == 0)
+            else if(arg3 == "D")
             {
                 lower_bound = (double)60;
             }
@@ -126,10 +129,10 @@ int main(int argc, const char *argv[])
                 lower_bound = (double)0;
             }
         }
-        else if(strcmp(argv[4], "C") == 0)
+        else if (arg4 == "C")
         {
             upper_bound = 79.9;
-            if(strcmp(argv[3], "D") == 0)
+            if(arg3 == "D")
             {
                 lower_bound = (double)60;
             }
@@ -138,15 +141,14 @@ int main(int argc, const char *argv[])
                 lower_bound = (double)0;
             }
         }
-        else if (strcmp(argv[4], "D") == 0)
+        else if (arg4 == "D")
         {
             upper_bound = 69.9;
         }
-        if (strcmp(argv[3], "F") == 0)
+        if(arg3 == "F")
         {
             lower_bound = 0;
-        }
-        
+        }        
         if (students_grades[i].average <= upper_bound && students_grades[i].average >= lower_bound)
         {
             outFile << students_grades[i].studentName << "," << students_grades[i].average << ",";
