5d4a774385f91f4ad32b52f1a8f73fb7478c3ef6
Bach Nguyen <73509508+Bach2324@users.noreply.github.com>
2021-04-02 00:45:51 -0600

    Completed assignment 7

--- assignment7/MovieTree.cpp
+++ assignment7/MovieTree.cpp
@@ -14,27 +14,49 @@ LLMovieNode* getLLMovieNode(int r, std::string t, int y, float q)
 	lmn->next =NULL;
 	return lmn;
 }
+TreeNode *getTreeNode(string title)			// Function to dynamically allocate a Tree Node
+{
+	TreeNode *in = new TreeNode();			// Dynamically allocate a new node
+	in->titleChar = title[0];			// Assign it's data value
+	return in;					// return the pointer
+}
 /* ------------------------------------------------------ */
 MovieTree::MovieTree()
 {
     root = NULL;
 }
 /* ------------------------------------------------------ */
-MovieTree::~MovieTree()
+void DeleteAllDS(TreeNode *del,	LLMovieNode *delLL)	// Helper function for destructor
 {
-
+	if (del == nullptr){return;}			// Return if pointer is null
+	DeleteAllDS(del->leftChild, del->head);		// Otherwise we will post-order traverse
+	DeleteAllDS(del->rightChild, del->head);	// Traverse right
+	while (del->head != nullptr)			// Entering the linked list portion
+	{						// Traverse the linked list
+		delLL = del->head->next;		// Assign the next head node to delLL
+		delete del->head;			// Delete current head node
+		del->head = delLL;			// Assign the new head node to what we stored in DelLL
+	}
+	delete del;					// delete/deallocate the tree node we are currently at
+}
+MovieTree::~MovieTree()					// Desctructor function
+{
+	DeleteAllDS(root, root->head);			// Call helper function to deallocate all Data Structure
 }
 /* ------------------------------------------------------ */
 void showMovieCollectionHelper(TreeNode *t)		// helper function Print movies according to given letter
 {
 	if(t == nullptr){return;}			// exit function call if t is null
 	showMovieCollectionHelper(t->leftChild);	// Otherwise, in-order traversal (left side)
-	cout << "Movies starting with letter: " << t->titleChar << endl;// Print the header of print statement and letter of movies
 	LLMovieNode *m = t->head;			// Create a new LL node that points to the head
-	while (m != nullptr)				// Traverse the linked list for that specific letter
-	{
-		cout << " >> " << m->title << " " << m->rating << endl;	// Print all movies that started with given letter follow by rating
-		m = m->next;				// Go to the next node
+	if (m != nullptr)				// Make sure we are not printing anything if 
+	{						// Print the header of print statement and letter of movies
+		cout << "Movies starting with letter: " << t->titleChar << endl;
+		while (m != nullptr)			// Traverse the linked list for that specific letter
+		{					// Print all movies that started with given letter follow by rating
+			cout << " >> " << m->title << " " << m->rating << endl;
+			m = m->next;			// Go to the next node
+		}
 	}
 	showMovieCollectionHelper(t->rightChild);	// In-order traversal (right side)
 }
@@ -114,54 +136,50 @@ void insertTreeNode(TreeNode *&t, TreeNode *&parent)	// Helper function to add n
 	}
 }
 void MovieTree::insertMovie(int ranking, string title, int year, float rating)
-{									// Main function to insert movie
-	TreeNode *search = new TreeNode;				// Dynamically allocate a search node for tree
-	TreeNode *t = new TreeNode;					// Dynamically allocate a t node for tree
-	t->titleChar = title[0];					// Assign data of tree node to be the first character of string title
-
-	LLMovieNode * LLnode = new LLMovieNode;				// Dynamically allocate a node for linked list
-	LLnode = getLLMovieNode(ranking, title, year, rating);		// get the information from the getLLMovieNode function and assign it to LLnode
-	
-	if (root == nullptr){root = t;}					// Assign t to the root if tree is empty
-	else{insertTreeNode(t, root);}					// Otherwise, we call our insertTreeNode function to insert the tree node
-
-	search = searchCharNode(title[0]);				// Call searchCharNode to get the pointer pointing to the node that contains key char
+{	
+	if (searchCharNode(title[0]) == NULL)		// Call searchChar node check if node containing the char exist
+	{						// If not we, need to allocate memory and insert a new tree NOde
+		TreeNode *t = getTreeNode(title);	// Call getTreeNode to create new node
+		insertTreeNode(t, root);		// Call insertTree helper function to insert tree node
+	}						// Otherwise, we call our insertTreeNode function to insert the tree node											// Main function to insert movie
+	TreeNode* search = searchCharNode(title[0]);	// Call searchCharNode to get the pointer pointing to the node that contains key char
+	LLMovieNode * LLnode = getLLMovieNode(ranking, title, year, rating);// get the information from the getLLMovieNode function and assign it to LLnode
 	if(search->titleChar == title[0]){insertLLNode(search,LLnode);} // If we found the node containing key char, call insertLLnode to insert node
 	else{insertLLNode(root,LLnode);}				// otherwise, we insert it at the root tree node
 }
 /* ------------------------------------------------------ */
 void MovieTree::removeMovieRecord(string title)
-{									// Evaluate if we are deleting from an empty tree
+{							// Evaluate if we are deleting from an empty tree
 	if (root == nullptr){cout << "Movie not found." << endl;return;}
 
-	TreeNode *search = searchCharNode(title[0]);			// Call search function to return the pointer to node containing key char and assign it to search
-	LLMovieNode *crawler = search->head;				// Declare a crawler pointer to traverse linked list, assign it as the head
-	LLMovieNode *previous = crawler;				// Declare a previous pointer to traverse behind crawler
+	TreeNode *search = searchCharNode(title[0]);	// Call search function to return the pointer to node containing key char and assign it to search
+	LLMovieNode *crawler = search->head;		// Declare a crawler pointer to traverse linked list, assign it as the head
+	LLMovieNode *previous = crawler;		// Declare a previous pointer to traverse behind crawler
 
-	if (search->head->title == title)				// If the node we are deleting is the head node of the linked list
+	if (search->head->title == title)		// If the node we are deleting is the head node of the linked list
 	{
-		if (crawler->next == nullptr)				// Then we evaluate to see if it's the only node or if there are several nodes
+		if (crawler->next == nullptr)		// Then we evaluate to see if it's the only node or if there are several nodes
 		{
-			delete crawler;					// If it's the only node, we delete it 
-			search->head = nullptr;				// and set the associated tree node to nullptr, indicating empty LL
+			delete crawler;			// If it's the only node, we delete it 
+			search->head = nullptr;		// and set the associated tree node to nullptr, indicating empty LL
 		}
-		else							// otherwise, if there are several nodes
+		else					// otherwise, if there are several nodes
 		{											
-			search->head = crawler->next;			// Assign the head to be the next node in the linked list
-			delete crawler;					// Delete the current node
+			search->head = crawler->next;	// Assign the head to be the next node in the linked list
+			delete crawler;			// Delete the current node
 		}
 	}
-	else								// Evaluate if we are not deleting at the head/front of the linked list
+	else						// Evaluate if we are not deleting at the head/front of the linked list
 	{
-		while (crawler != nullptr)				// Traverse the Linked List
+		while (crawler != nullptr)		// Traverse the Linked List
 		{
-			if (crawler->title == title)			// If we found the node that contains the title we are looking for
+			if (crawler->title == title)	// If we found the node that contains the title we are looking for
 			{
-				previous->next = crawler->next;		// Assign the current node pointer will be the next node
-				delete crawler;				// Delete the current node
+				previous->next = crawler->next;	// Assign the current node pointer will be the next node
+				delete crawler;		// Delete the current node
 			}
-			previous = crawler;				// Iterate previous
-			crawler = crawler->next;			// Iterate crawler
+			previous = crawler;		// Iterate previous
+			crawler = crawler->next;	// Iterate crawler
 		}
 	}
 }
@@ -183,3 +201,25 @@ TreeNode* MovieTree::searchCharNode(char key)
     return searchCharHelper(root, key);
 }
 /* ------------------------------------------------------ */
+void MovieTree::leftRotation(TreeNode* curr)		// Function to rotate tree
+{											
+	TreeNode* y = curr->rightChild;			// Declare a new pointer and point it to right child of curr
+	curr->rightChild = y->leftChild;		// assign curr right child to be y's left child
+	
+	if (y->leftChild != NULL)			// Check to see if y is null
+	{
+		y->leftChild->parent = curr;		// If not, then we assign y's leftchild's parent as curr
+	}
+	y->parent = curr->parent;			// Assign y parent as curr's parent
+	if (curr->parent == NULL){root = y;}		// if curr's parent is null, assign root as y
+	else						// Otherwise, we check for other conditions
+	{
+		if (curr == curr->parent->leftChild)	// if the current node is the left child's parent
+		{
+			curr->parent->leftChild = y;	// Then we assign it as y
+		}
+		else{curr->parent->rightChild = y;}	// Otherwise, we assign rightchild's as y
+	}
+	y->leftChild = curr;				// At the end, y's left child is curr
+	curr->parent = y;				// curr's parent is y
+}
