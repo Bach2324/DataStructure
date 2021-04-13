246882a74db148e8900b2829de8e05e772c0509d
Bach Nguyen <73509508+Bach2324@users.noreply.github.com>
2021-04-12 21:26:29 -0600

    Fixing comment format

--- exam2/Q2/bst.cpp
+++ exam2/Q2/bst.cpp
@@ -21,27 +21,27 @@ void BST::deleteTree(Node* node) {
 ** Implement the following function to return the count of comparisons, 
 **   you may implement helper functions.
 */
-int searchCounterHelper(Node *search, int target)// Helper function to return counter
+int searchCounterHelper(Node *search, int target)			// Helper function to return counter
 {
-	int counter = 0;							// Declare a counter variable
+	int counter = 0;						// Declare a counter variable
 	while (search != nullptr)					// iteratively traverse the BST
 	{
 		if (search->key > target)				// Go left if target is less than node's value
 		{
-			counter++;							// Count the comparison made
+			counter++;					// Count the comparison made
 			search = search->leftChild;			// iterate/update the pointer
 		}
 		if (search->key < target)				// Go right if target is greater than node's value
 		{
-			counter++;							// Count the comparison
-			search = search->rightChild;		// iterate/update the pointer
-		}										// If we found the node, then update the comparison and break out of loop
+			counter++;					// Count the comparison
+			search = search->rightChild;			// iterate/update the pointer
+		}							// If we found the node, then update the comparison and break out of loop
 		else if (search->key == target){counter++; break;}
 	}
-	return counter;								// return counter
+	return counter;							// return counter
 }
-int BST::searchCounter(int target)				// Main search Counter function
+int BST::searchCounter(int target)					// Main search Counter function
 {	
-	return searchCounterHelper(root, target);	// Call the helper function to solve code
+	return searchCounterHelper(root, target);			// Call the helper function to solve code
 	// your code here!
 }
